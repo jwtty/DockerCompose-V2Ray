c7a9a5e945fb9006066fe429fe3bee6d1528f5c0
David Lee <daviddwlee84@gmail.com>
2023-08-08 10:41:25 +0800

    Fix typo of initial_https.sh

--- initial_https.sh
+++ initial_https.sh
@@ -3,7 +3,7 @@
 your_domain="your_domain"
 your_email_address="your_email_address"
 
-sed -i "s/your_domain/$your_domain/" data/nginx/conf.d/core_trader.conf
+sed -i "s/your_domain/$your_domain/" data/nginx/conf.d/v2ray.conf
 sed -i "s/your_domain/$your_domain/" init-letsencrypt.sh
 sed -i "s/your_email_address/$your_email_address/" init-letsencrypt.sh
 
